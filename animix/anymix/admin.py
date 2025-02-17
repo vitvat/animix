4801957236fcc2dfce660209e1cda39a520dde27
vitvat <vitvat@ua.fm>
2025-02-17 19:51:46 +0200

    3

--- animix/anymix/admin.py
+++ animix/anymix/admin.py
@@ -1,3 +1,27 @@
+from django import forms
 from django.contrib import admin
+from .models import Gen, Creature
 
-# Register your models here.
+
+class CreatureForm(forms.ModelForm):
+    class Meta:
+        model = Creature
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['father'].widget.can_add_related = False
+        self.fields['mother'].widget.can_add_related = False
+
+
+class GenAdmin(admin.ModelAdmin):
+    list_display = ('name', 'rank', 'type')
+
+
+class CreatureAdmin(admin.ModelAdmin):
+    form = CreatureForm
+    list_display = ('name', 'father', 'mother', 'rank', 'type')
+
+
+admin.site.register(Gen, GenAdmin)
+admin.site.register(Creature, CreatureAdmin)
--- animix/anymix/models.py
+++ animix/anymix/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Gen(models.Model):
+    TYPE_CHOICES = [
+        ('gold', 'Gold'),
+        ('water', 'Water'),
+        ('air', 'Air'),
+        ('universal', 'Universal'),
+    ]
+
+    name = models.CharField(max_length=25)
+    rank = models.IntegerField(choices=[(i, i) for i in range(2, 8)])
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    mother_only = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+
+class Creature(models.Model):
+    TYPE_CHOICES = [
+        ('gold', 'Gold'),
+        ('water', 'Water'),
+        ('air', 'Air'),
+        ('universal', 'Universal'),
+    ]
+
+    name = models.CharField(max_length=25)
+    father = models.ForeignKey(Gen, on_delete=models.CASCADE, related_name='creature_father')
+    mother = models.ForeignKey(Gen, on_delete=models.CASCADE, related_name='creature_mother')
+    rank = models.IntegerField(choices=[(i, i) for i in range(2,11)])
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def clean(self):
+        if self.father == self.mother:
+            raise ValidationError('Father and mother cannot be the same Gen.')
+        if self.mother.father_only:
+            raise ValidationError('This Gen can only be a father.')
+
+    def __str__(self):
+        # Метод для отображения объекта в виде строки, возвращает название
+        return self.name
+
+    class Meta:
+        ordering = ['id']  # Упорядочить записи по возрастанию id
